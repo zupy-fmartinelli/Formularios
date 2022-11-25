78073f1f712668dc5109f2d4d2613e3995648df0
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-25 10:56:55 -0300

    Validando Datas

--- passagens/forms.py
+++ passagens/forms.py
@@ -22,10 +22,15 @@ class PassagemForms(forms.Form):
     def clean(self):
         origem = self.cleaned_data.get('origem')
         destino = self.cleaned_data.get('destino')
+        data_ida = self.cleaned_data.get('data_ida')
+        data_volta = self.cleaned_data.get('data_volta')
+        data_pesquisa = self.cleaned_data.get('data_pesquisa')
         lista_de_erros = {}
         campo_tem_algum_numero(origem, 'origem', lista_de_erros)
         campo_tem_algum_numero(destino, 'destino', lista_de_erros)
         origem_destino_iguais(origem, destino, lista_de_erros)
+        data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros)
+        data_ida_menor_que_data_hoje(data_ida, data_pesquisa, lista_de_erros)
         if lista_de_erros is not None:
             for erro in lista_de_erros:
                 mensagem_erro = lista_de_erros[erro]
--- passagens/validation.py
+++ passagens/validation.py
@@ -7,3 +7,16 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
     """ Verifica se possui algum digito numérico """
     if any(char.isdigit() for char in valor_campo):
         lista_de_erros[nome_campo] = 'Não inclua números neste campo'
+        
+def data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
+    """ Verifica se data de ida é maior que data de volta """
+    if data_ida > data_volta:
+        lista_de_erros['data_volta'] = 'Data de ida não pode ser maior que data de volta'
+        
+        
+def data_ida_menor_que_data_hoje(data_ida, data_pesquisa, lista_de_erros):
+    """ Verifica se data de ida é menor que hoje """
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que hoje'
+        
+        
