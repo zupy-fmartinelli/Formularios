69ac27a02d098d564a5ab79cf54efc6f82a1dde6
Fabio Martinelli <webmaster@zupy.com.br>
2022-11-25 09:54:36 -0300

    Validando forms

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,7 +1,26 @@
 from django import forms
+from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from passagens.classe_viagem import tipos_de_classe
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100)
     destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida')
-    data_volta = forms.DateField(label='Volta')
+    data_ida = forms.DateField(label='Ida', widget=DatePicker())
+    data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da Pesquisa', disabled=True, initial=datetime.today)
+    classe_viagem = forms.ChoiceField(label='Classe do Vôo', choices=tipos_de_classe)
+    informacoes = forms.CharField(
+        label='Informações extras',
+        max_length=200,
+        widget=forms.Textarea(),
+        required=False
+    )
+    email = forms.EmailField(label='Email', max_length=150)
+    
+    def clean_origem(self):
+        origem = self.cleaned_data.get('origem')
+        if any(char.isdigit() for char in origem):
+            raise forms.ValidationError('Origem inválida: Não inclusa números')
+        else:
+            return origem
--- setup/settings.py
+++ setup/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "passagens",
+    "tempus_dominus",
+    'widget_tweaks',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -123,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+TEMPUS_DOMINUS_LOCALIZE = True
